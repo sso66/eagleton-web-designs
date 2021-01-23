7032e8cdbca2217d51167655871de3c507b10392
Stephen <sun.stephen.oo@gmail.com>
2021-01-23 08:44:07 -0500

    get started with material-ui components

--- src/features/theme.js
+++ src/features/theme.js
@@ -2,7 +2,7 @@
 import { red } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
-// A custom theme for this app
+// ___ custom theme for this app
 const theme = createMuiTheme({
   palette: {
     primary: {
